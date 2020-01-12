35da1bb23d9750afc3d93b192617b123780ed0aa
Pedro Coelho <pedrogonic@gmail.com>
2020-01-12 11:47:18 -0300

    End of lesson

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -2,5 +2,6 @@ package guru.springframework;
 
 public interface Expression {
     Money reduce(Bank bank, String to);
-    public Expression plus(Expression addend);
+    Expression plus(Expression addend);
+    Expression times(int multiplier);
 }
