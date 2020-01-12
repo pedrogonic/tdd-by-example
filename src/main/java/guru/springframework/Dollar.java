caaa9cda63548e5d7ce04ba5c2bb324b31f0cbcc
Pedro Coelho <pedrogonic@gmail.com>
2020-01-11 23:00:08 -0300

    End of lesson

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -2,9 +2,7 @@ package guru.springframework;
 
 import java.util.Objects;
 
-public class Dollar {
-
-    private int amount;
+public class Dollar extends Money {
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -14,16 +12,4 @@ public class Dollar {
         return new Dollar(amount * multiplier);
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Dollar dollar = (Dollar) o;
-        return amount == dollar.amount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
-    }
 }
---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,21 @@
+package guru.springframework;
+
+import java.util.Objects;
+
+public class Money {
+
+    protected int amount;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Money money = (Money) o;
+        return amount == money.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
+}
